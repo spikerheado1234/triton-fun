f05e7f21d7a1de3926a26f06b100530da9c4d6ae
Ahan Gupta <ag82@charithm-serv-gpu-01.cs.illinois.edu>
2024-04-07 23:54:09 -0500

    working vector_addition

--- vector_addition.py
+++ vector_addition.py
@@ -1,31 +1,35 @@
 import torch
 import triton
 import triton.language as tl
-
+import nvtx
 
 @triton.jit
-def vector_add_kernel(x,y,
-                        output,elements,
-                        BLOCK_SIZE = tl.constexpr):
-    
+def vector_add_kernel(x_ptr,
+                      y_ptr,
+                      output_ptr,
+                      n_elements,
+                      BLOCK_SIZE : tl.constexpr,
+                      ):
+
     ## Get blockIdx.
     blockIdx = tl.program_id(axis=0)
 
     # Get all the pointers to index the inputs.
     offsets = blockIdx * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
 
-    mask = offsets < elements 
+    mask = offsets < n_elements 
 
-    x_values = tl.load(x + offsets, mask=mask)
-    y_values = tl.load(y+offsets, mask=mask)
+    x_values = tl.load(x_ptr+offsets, mask=mask)
+    y_values = tl.load(y_ptr+offsets, mask=mask)
 
     output_valules = x_values + y_values
 
-    tl.store(output + offsets, output_valules, mask=mask)
+    tl.store(output_ptr + offsets, output_valules, mask=mask)
 
 
+@nvtx.annotate("triton_vector_add", color="purple")
 def vector_add_launcher(x : torch.Tensor, y : torch.Tensor, 
-                        GPU_ID : int, BLOCK_SIZE = 1024):
+                        GPU_ID : int, b_size = 1024):
 
     output = torch.empty_like(x).to(GPU_ID)
 
@@ -35,15 +39,35 @@ def vector_add_launcher(x : torch.Tensor, y : torch.Tensor,
 
     assert x.is_cuda and y.is_cuda and output.is_cuda, "Tensors must be on GPU." 
 
-    grid = (elements, BLOCK_SIZE)
-
-    vector_add_kernel[grid](x,y,output,BLOCK_SIZE=BLOCK_SIZE)
+    ## For some reason, this doesn't work.
+    #grid = (triton.cdiv(elements, BLOCK_SIZE),)
+    grid = lambda meta : (triton.cdiv(elements, meta['BLOCK_SIZE']), )
+    vector_add_kernel[grid](x,y,output,elements,BLOCK_SIZE=b_size)
+    #add_kernel[grid](x,y,output,elements,BLOCK_SIZE=b_size)
     
     return output 
 
 
+import time
+
+@nvtx.annotate("torch_add", color="yellow")
+def torch_add(x: torch.Tensor, y : torch.Tensor):
+    return x+y
+
 BLOCK_SIZE=1024
-size = 10
+size = int(1e5)
 x = torch.rand(size, device='cuda')
 y = torch.rand(size, device='cuda')
+torch.cuda.synchronize()
+trit_start = time.time()
 out = vector_add_launcher(x,y,0,BLOCK_SIZE)
+torch.cuda.synchronize()
+trit_end = time.time()
+## Question is: what does this do? Launch another kernel or what?
+torch.cuda.synchronize()
+start = time.time()
+torch_result = torch_add(x,y)
+torch.cuda.synchronize()
+end = time.time()
+print(f'absolute error: {torch.max(torch.abs(torch_result - out))}')
+print(f'time taken torch: {end-start:.3f} time taken triton: {trit_end-trit_start:.3f}')
