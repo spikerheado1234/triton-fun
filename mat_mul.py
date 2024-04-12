c9f3f6b1bb03f0aff392eee8c1d929f7ef6bf6bc
Ahan Gupta <ag82@charithm-serv-gpu-01.cs.illinois.edu>
2024-04-11 19:31:11 -0500

    correct(ish) matmul in triton

--- mat_mul.py
+++ mat_mul.py
@@ -3,6 +3,8 @@
 import triton
 import triton.language as tl
 import torch
+import nvtx
+import pdb
 
 ### Here we are doing an: m * k by k * n mat-mul.
 @triton.jit
@@ -13,46 +15,48 @@ def mat_mul_kernel(x_ptr, y_ptr, output_ptr, m,n,k, BLOCK_SIZE: tl.constexpr):
     blockIdxY = tl.program_id(axis=1)
 
     ## How we will do is this is to load a tile and multiply.
-    inner_tile_dim = 128
+    inner_tile_dim : tl.constexpr = 128
 
     ## This is a BLOCK_SIZE by inner_tile_dim block of pointers.
-    x_ptrs = x_ptr + (blockIdxY*BLOCK_SIZE + (tl.arange(0, BLOCK_SIZE)*k))[:,None] + tl.arange(0, inner_tile_dim)[None,:]
+    x_ptrs = (blockIdxY*BLOCK_SIZE*k + (tl.arange(0, BLOCK_SIZE)[:,None]*k)) + tl.arange(0, inner_tile_dim)[None,:]
     
     ## This is an inner_tile_dim by BLOCK_SIZE block of pointers.
-    y_ptrs = y_ptr + (tl.arange(0, inner_tile_dim)[:,None] * n) + tl.arange(0, BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE 
+    y_ptrs = (tl.arange(0, inner_tile_dim)[:,None] * n) + tl.arange(0, BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE 
 
     ## Where we compute partial MACs.
     accumulator = tl.zeros((BLOCK_SIZE, BLOCK_SIZE), dtype=tl.float32)
 
     ## This is for masking within the loop itself.
     masked_k = tl.arange(0, inner_tile_dim)
+    #pdb.set_trace()
 
     for i in range(tl.cdiv(k, inner_tile_dim)):
 
         ## Masking now becomes a tad complex.
-        masked_x_ptrs = masked_k[None,:] * i*inner_tile_dim < k
-        masked_x_ptrs = masked_x_ptrs or (tl.arange(0, BLOCK_SIZE)[:,None] + blockIdxY*BLOCK_SIZE < m)
-        masked_y_ptrs = masked_k[:,None] * i*inner_tile_dim < k
-        masked_y_ptrs = masked_y_ptrs or (tl.arange(0,BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE < n)
+        masked_x_ptrs = masked_k[None,:] + i*inner_tile_dim < k
+        masked_y_ptrs = masked_k[:,None] + i*inner_tile_dim < k
+        ## For broacast semantics we need to use * operator as a pairwise boolean AND between the two tensors.
+        masked_x_ptrs = masked_x_ptrs * (tl.arange(0, BLOCK_SIZE)[:,None] + blockIdxY*BLOCK_SIZE < m) 
+        masked_y_ptrs = masked_y_ptrs * (tl.arange(0,BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE < n)
 
-        x_tile = tl.load(x_ptrs, x_ptr, mask=masked_x_ptrs, other=0.0)
-        y_tile = tl.load(y_ptrs, y_ptr, mask=masked_y_ptrs, other=0.0)
+        x_tile = tl.load(x_ptr+x_ptrs, mask=masked_x_ptrs, other=0.0)
+        y_tile = tl.load(y_ptr+y_ptrs, mask=masked_y_ptrs, other=0.0)
 
         accumulator += tl.dot(x_tile, y_tile)
 
         ## Compute the correct new indices.
-        x_ptrs += inner_tile_dim*k
+        x_ptrs += inner_tile_dim
         y_ptrs += inner_tile_dim*n
 
     ## Compute where to store the answer here.
     ## This gets a 1024 by 1024 block of data 
     ## to store back to memory.
-    c_ptrs = output_ptr + tl.arange(0,BLOCK_SIZE)[:,None]*n + tl.arange(0, BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE + blockIdxY*BLOCK_SIZE*n
+    c_ptrs = tl.arange(0,BLOCK_SIZE)[:,None]*n + tl.arange(0, BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE + blockIdxY*BLOCK_SIZE*n
 
     ## And the x and y boundary conditions.
     c_mask = tl.arange(0, BLOCK_SIZE)[None,:] + blockIdxX*BLOCK_SIZE < n 
-    c_mask = c_mask or (tl.arange(0, BLOCK_SIZE)[:,None]*n + blockIdxY*BLOCK_SIZE < m)
-    tl.store(c_ptrs, accumulator, mask=c_mask)
+    c_mask = c_mask * (tl.arange(0, BLOCK_SIZE)[:,None] + blockIdxY*BLOCK_SIZE < m)
+    tl.store(output_ptr + c_ptrs, accumulator, mask=c_mask)
 
 @nvtx.annotate("triton-matmul", color="purple")
 def mat_mul_launcher(x : torch.Tensor, y : torch.Tensor, GPU_ID : int, BLOCK_SIZE):
@@ -66,24 +70,25 @@ def mat_mul_launcher(x : torch.Tensor, y : torch.Tensor, GPU_ID : int, BLOCK_SIZ
     output = torch.empty((m,n), dtype=torch.float32).to(GPU_ID)
 
     k = k_one
-    BLOCK_SIZE = 128
 
     grid = (triton.cdiv(m,BLOCK_SIZE), triton.cdiv(n, BLOCK_SIZE))
 
-    mat_mul_kernel[grid](x,y,output,m,n,k, BLOCK_SIZE=BLOCK_SIZE, num_warps=4)
+    mat_mul_kernel[grid](x,y,output,m,n,k, BLOCK_SIZE=BLOCK_SIZE)
+    
+    return output
 
 
-@nvtx.annotate("torch-einsum", color="yello")
+@nvtx.annotate("torch-einsum", color="yellow")
 def torch_matmul(x : torch.Tensor, y : torch.Tensor):
     return torch.einsum('ab,bc -> ac', x, y)
 
 
 ## We spawn a BLOCK_SIZE x BLOCK_SIZE GPU tile.
-BLOCK_SIZE = 128
+BLOCK_SIZE = 16
 GPU_ID = 0
-n = 10
-m = 10
-k = 10
+n = 32
+m = 32
+k = 32
 x = torch.rand((m,k), dtype=torch.float32).to(GPU_ID)
 y = torch.rand((k, n), dtype=torch.float32).to(GPU_ID)
 
@@ -92,6 +97,6 @@ out_torch = torch_matmul(x,y)
 out_triton = mat_mul_launcher(x,y,GPU_ID, BLOCK_SIZE)
 torch.cuda.synchronize()
 
-print(f'tensors equal: {torch.equal(out_torch, out_triton)}')
+print(f'absolute max error: {torch.max(torch.abs(out_torch - out_triton))}')
 
 
